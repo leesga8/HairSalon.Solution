eb8525abd17756abe9b61eddaef6c7ff7b5616b4
Albert Lee <leesga8@gmail.com>
2021-12-29 19:08:05 -0800

    add DateHired, stylists model

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -1,16 +1,27 @@
 using System.Collections.Generic;
+using System;
 
 namespace HairSalon.Models
 {
-    public class Stylist
+  public class Stylist
+  {
+    public Stylist()
     {
-        public Stylist()
-        {
-            this.Clients = new HashSet<Client>();
-        }
-
-        public int StylistId { get; set; }
-        public string Name { get; set; }
-        public virtual ICollection<Client> Clients { get; set; }
+      this.Clients = new HashSet<Client>();
     }
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+    public DateTime DateHired { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
+
+    //   public Specialty StylistSpecialty { get; set; }
+    // }
+    // public enum Specialty
+    // {
+    //   Perm,
+    //   Balage
+    //   // Curly,
+    //   // Fade
+    // }
+  }
 }
